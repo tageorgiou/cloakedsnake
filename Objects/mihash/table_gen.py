8be63b93f0c154619435e8937520eb8fb266792e
Pedram <scarllati@gmail.com>
2012-05-14 23:21:52 -0400

    fixed the table values

--- Objects/mihash/table_gen.py
+++ Objects/mihash/table_gen.py
@@ -1,9 +1,10 @@
 import random
-res = []
-for i in range(0, 256, 1):
-    res.append(random.randint(-2**63+1, 2**63-1))
+
 for j in range(8):
     print "long table" + str(j) + "[256] = {"
+    res = []
+    for i in range(0, 256, 1):
+    	res.append(random.randint(-2**63+1, 2**63-1))
     for i in range(0, len(res), 8):
         str_q = str(res[i:i+8])[1 : -1]
         print str_q + ", \\"
