98a575bd8e439027bcb7b9700e09924223343faa
Thomas Georgiou <tageorgiou@gmail.com>
2012-05-16 20:31:01 -0400

    Better rand generator params and more randnums

--- Objects/generaterandtable.py
+++ Objects/generaterandtable.py
@@ -1,14 +1,17 @@
 randnums = open('randnums','r').read().split()
 randgenerator = randnums.__iter__()
-print "unsigned char randchartable[%d] = {" % (16 * 256 * 8)
+tables = 16
+tablesize = 2**8
+tableentrylength = 8
+print "unsigned char randchartable[%d] = {" % (tables * tablesize * tableentrylength)
 
-for i in range(0,16):
+for i in range(0,tables):
     print "\t//table %d" % i
     #8 tables
-    for j in range(0,256):
+    for j in range(0,tablesize):
         #256 entries per table
         print "\t",
-        for k in range(0,8):
+        for k in range(0,tableentrylength):
            print "%3d," % int(randgenerator.next()),
         print ""
 print "};"
